851913d4a283dea33abe83ea1e457c6ccb4f47c4
David Larsson <david.l.larsson.90@gmail.com>
2020-05-08 14:02:04 +0200

    Add project files.

---
+++ GraphDataStructureAndDijkstra/Vertex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GraphDataStructureAndDijkstra
+{
+    public class Vertex
+    {
+        public bool Visited { get; set; }
+        public string Name { get; set; }
+
+        public Vertex(string name)
+        {
+            Name = name;
+            Visited = false;
+        }
+    }
+}
