18606df85ee6b1bfa6ed45609bb42c5ba73f60f8
David Larsson <david.l.larsson.90@gmail.com>
2020-05-11 15:30:04 +0200

    Added Forms app and graphs graphics

---
+++ FormsApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace FormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawArea = new System.Windows.Forms.PictureBox();
+            this.brnGenerateGraph = new System.Windows.Forms.Button();
+            this.tbNumberOfVertices = new System.Windows.Forms.TextBox();
+            this.lblNumberOfVertices = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.drawArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawArea
+            // 
+            this.drawArea.Location = new System.Drawing.Point(131, 12);
+            this.drawArea.Name = "drawArea";
+            this.drawArea.Size = new System.Drawing.Size(1241, 687);
+            this.drawArea.TabIndex = 0;
+            this.drawArea.TabStop = false;
+            // 
+            // brnGenerateGraph
+            // 
+            this.brnGenerateGraph.Location = new System.Drawing.Point(12, 57);
+            this.brnGenerateGraph.Name = "brnGenerateGraph";
+            this.brnGenerateGraph.Size = new System.Drawing.Size(113, 23);
+            this.brnGenerateGraph.TabIndex = 1;
+            this.brnGenerateGraph.Text = "Generate Graph";
+            this.brnGenerateGraph.UseVisualStyleBackColor = true;
+            this.brnGenerateGraph.Click += new System.EventHandler(this.btn_GenerateGraph);
+            // 
+            // tbNumberOfVertices
+            // 
+            this.tbNumberOfVertices.Location = new System.Drawing.Point(12, 31);
+            this.tbNumberOfVertices.Name = "tbNumberOfVertices";
+            this.tbNumberOfVertices.Size = new System.Drawing.Size(113, 20);
+            this.tbNumberOfVertices.TabIndex = 2;
+            // 
+            // lblNumberOfVertices
+            // 
+            this.lblNumberOfVertices.AutoSize = true;
+            this.lblNumberOfVertices.Location = new System.Drawing.Point(13, 12);
+            this.lblNumberOfVertices.Name = "lblNumberOfVertices";
+            this.lblNumberOfVertices.Size = new System.Drawing.Size(99, 13);
+            this.lblNumberOfVertices.TabIndex = 3;
+            this.lblNumberOfVertices.Text = "Number of vertices:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 86);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(113, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 711);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblNumberOfVertices);
+            this.Controls.Add(this.tbNumberOfVertices);
+            this.Controls.Add(this.brnGenerateGraph);
+            this.Controls.Add(this.drawArea);
+            this.Name = "Form1";
+            this.Text = "ROUTE CITY";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.drawArea)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawArea;
+        private System.Windows.Forms.Button brnGenerateGraph;
+        private System.Windows.Forms.TextBox tbNumberOfVertices;
+        private System.Windows.Forms.Label lblNumberOfVertices;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ FormsApp/Form1.cs
@@ -0,0 +1,121 @@
+﻿using GraphDataStructureAndDijkstra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsApp
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics drawGraphics;
+
+        Graph g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            drawGraphics = drawArea.CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_GenerateGraph(object sender, EventArgs e)
+        {
+            GraphGenerator gg = new GraphGenerator();
+            Graph g = gg.Generate(int.Parse(tbNumberOfVertices.Text));
+            this.g = g;
+            var coords = getRandomCoords(g.NumberOfVertices);
+            Pen pen = new Pen(Color.Black);
+            Pen pen2 = new Pen(Color.Red);
+            Pen pen3 = new Pen(Color.Blue);
+
+
+            for (int i = 0; i < g.Vertices.Count; i++)
+            {
+                int[] xy = coords[i];
+
+                var vertexName = g.Vertices[i].Name;
+
+                drawGraphics.DrawEllipse(pen, xy[0], xy[1], 100, 100);
+                drawGraphics.DrawString(vertexName, new System.Drawing.Font("Arial", 30), pen2.Brush, xy[0] + 25, xy[1] + 25);
+
+
+             
+            }
+            for (int k = 0; k < g.NumberOfVertices; k++)
+            {
+                for (int l = 0; l < g.NumberOfVertices; l++)
+                {
+                    if (g.AdjacenyMatrix[k, l] > 0) //Edge between vertex k and vertex l
+                    {
+                        int[] kVertex = coords[k];
+                        int[] lVertex = coords[l];
+                        int weightX = 0;
+                        int weightY = 0;
+                        if (lVertex[0] > kVertex[0])
+                        {
+                            weightX = (kVertex[0] + lVertex[0]) / 2;
+                             weightY = (kVertex[1] + lVertex[1]) / 2;
+                        }
+                        else
+                        {
+                             weightX = (lVertex[0] + kVertex[0]) / 2;
+                             weightY = (lVertex[1] + kVertex[1]) / 2;
+                        }
+                        int weight = g.AdjacenyMatrix[k, l];
+
+                        drawGraphics.DrawString(weight.ToString(), new System.Drawing.Font("Arial", 30), pen3.Brush, weightX, weightY);
+
+                        drawGraphics.DrawLine(pen, kVertex[0] + 50, kVertex[1] + 50, lVertex[0] + 50, lVertex[1] + 50);
+                    }
+                }
+            }
+
+        }
+
+        private List<int[]> getRandomCoords(int numberOfVertices)
+        {
+            Random r = new Random();
+            List<int[]> coords = new List<int[]>();
+            List<int> xCoords = new List<int>();
+            List<int> yCoords = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                xCoords.Add(i);
+                yCoords.Add(i);
+            }
+            for (int i = 0; i < numberOfVertices; i++)
+            {
+                var x = xCoords[r.Next(xCoords.Count)];
+                var y = yCoords[r.Next(yCoords.Count)];
+
+                xCoords.Remove(x);
+                yCoords.Remove(y);
+
+                coords.Add(new int[] { x * 50, y * 50 });
+
+            }
+
+
+            return coords;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.g = null;
+            drawArea.Image = null;
+        }
+    }
+}
