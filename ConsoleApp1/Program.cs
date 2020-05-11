5cf6d943e772409af397ca6a3f3c3cf7500a2745
David Larsson <david.l.larsson.90@gmail.com>
2020-05-11 12:46:13 +0200

    Added search function and dijkstras

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using GraphDataStructureAndDijkstra;
 using System;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,10 @@ namespace ConsoleApp1
             while (true)
             {
                 GraphGenerator gg = new GraphGenerator();
-                Graph g = gg.Generate(5);
+                Graph g = gg.Generate(10);
+
+
+
 
                 foreach (var item in g.PrintGraph())
                 {
@@ -20,9 +24,53 @@ namespace ConsoleApp1
                 {
                     Console.WriteLine(item);
                 }
+                var startVertex = int.Parse(Console.ReadLine());
+                var endVertex = int.Parse(Console.ReadLine());
+                Dijkstra d = new Dijkstra(g, startVertex, endVertex);
+
+                Console.WriteLine( d.SearchFromTo(startVertex, endVertex));
+                for (int i = 0; i < d.ShortestDistances.Length; i++)
+                {
+                    Console.WriteLine("vertex: " + g.Vertices[i].Name + "  Distance: " + d.ShortestDistances[i] + "    via vertex: + " + g.Vertices[d.PreviousVertices[i]].Name);
+                }
+                
 
                 Console.ReadLine();
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//Graph g = new Graph(5);
+
+//g.AddVertex("A");
+//g.AddVertex("B");
+//g.AddVertex("C");
+//g.AddVertex("D");
+//g.AddVertex("E");
+//g.AddEdge(0, 1, 6);
+//g.AddEdge(0, 3, 1);
+//g.AddEdge(1, 0, 6);
+//g.AddEdge(1, 2, 5);
+//g.AddEdge(1, 3, 2);
+//g.AddEdge(1, 4, 2);
+//g.AddEdge(2, 1, 5);
+//g.AddEdge(2, 4,5);
+//g.AddEdge(3, 0, 1);
+//g.AddEdge(3, 1, 2);
+//g.AddEdge(3, 4, 1);
+//g.AddEdge(4, 1, 2);
+//g.AddEdge(4, 2, 5);
+//g.AddEdge(4, 3, 1);
+
---
+++ GraphDataStructureAndDijkstra/Dijkstra.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace GraphDataStructureAndDijkstra
+{
+    public class Dijkstra
+    {
+        public int[] ShortestDistances { get; set; }
+        public int[] PreviousVertices { get; set; }
+        public List<int> unvisitedVertices { get; set; } = new List<int>();
+        public Graph Graph { get; set; }
+
+        public Dijkstra(Graph graph, int startVertex, int endVertex)
+        {
+            Graph = graph;
+            ShortestDistances = new int[graph.NumberOfVertices];
+            PreviousVertices = new int[graph.NumberOfVertices];
+            for (int i = 0; i < graph.NumberOfVertices; i++)
+            {
+                unvisitedVertices.Add(i);
+                ShortestDistances[i] = int.MaxValue;
+            }
+            ShortestDistances[startVertex] = 0;
+
+            while (unvisitedVertices.Count > 0)
+            {
+                var currentVertex = GetNextVertex();
+                for (int i = 0; i < graph.NumberOfVertices; i++)
+                {
+                    if (graph.AdjacenyMatrix[currentVertex, i] > 0)
+                    {
+                        if (ShortestDistances[i] > ShortestDistances[currentVertex] + graph.AdjacenyMatrix[currentVertex, i])
+                        {
+                            ShortestDistances[i] = ShortestDistances[currentVertex] + graph.AdjacenyMatrix[currentVertex, i];
+                            PreviousVertices[i] = currentVertex;
+                        }
+                    }
+                }
+            }
+        }
+
+        private int GetNextVertex()
+        {
+            var smallestKnownDistance = int.MaxValue;
+            var vertex = -1;
+            foreach (var value in unvisitedVertices)
+            {
+                if (ShortestDistances[value] <= smallestKnownDistance)
+                {
+                    smallestKnownDistance = ShortestDistances[value];
+                    vertex = value;
+                }
+            }
+            unvisitedVertices.Remove(vertex);
+            return vertex;
+        }
+
+        public StringBuilder SearchFromTo(int start, int end)
+        {
+            // Console.WriteLine("vertex: " + g.Vertices[i].Name + "  Distance: " + d.ShortestDistances[i] + "    via vertex: + " + g.Vertices[d.PreviousVertices[i]].Name);
+            var currentVertex = end;
+            List<string> path = new List<string>();
+            StringBuilder sb = new StringBuilder("Path: ");
+            path.Add(Graph.Vertices[end].Name);
+            while (currentVertex != start)
+            {
+                path.Add(Graph.Vertices[PreviousVertices[currentVertex]].Name);
+
+                currentVertex = Graph.Vertices.IndexOf(Graph.Vertices[PreviousVertices[currentVertex]]);
+            }
+            for (int i = path.Count -1; i > -1; i--)
+            {
+                sb.Append(" - " + path[i]);
+            }
+            sb.Append("  Total cost: " + ShortestDistances[end]);
+            return sb;
+        }
+
+    }
+}
